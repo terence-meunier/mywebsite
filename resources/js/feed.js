8925ce8f04130e733175461045043130ad447621
terence-meunier <terence.meunier@gmail.com>
2021-03-25 16:30:01 +0100

    fix(feed): add ajax fail and always method

--- resources/js/feed.js
+++ resources/js/feed.js
@@ -1,6 +1,7 @@
 $(function() {
     let ghibliAPI = "https://ghibliapi.herokuapp.com/films/";
-    $.getJSON(ghibliAPI).done(function(datas) {
+    $.getJSON(ghibliAPI)
+    .done(function(datas) {
         
         function getData(element) {
 
@@ -23,5 +24,13 @@ $(function() {
         }
         
         datas.map(getData);
+    })
+    .fail(function(error) {
+        console.log("La requête s'est terminée en échec.");
+        console.log("Status code: " + error.status);
+        console.log("Status text : " + error.statusText);
+    })
+    .always(function() {
+        console.log("Requête effectuée");
     });
 });
