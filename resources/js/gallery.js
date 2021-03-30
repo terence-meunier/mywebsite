d5c90433779da6f211d1d38adc5d1a50f0d80650
Térence <terence.meunier@gmail.com>
2021-03-30 16:12:21 +0200

    feat(gallery): add picture in the gallery with the form fixes #18

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -34,3 +34,15 @@ function toggleFormImg() {
 
 // Event on click button addImgForm
 document.querySelector('#addImgForm').addEventListener('click', () => toggleFormImg());
+
+// Récupération de l'url dans le champ de texte quand on clique sur le bouton du formulaire
+document.querySelector('#addImgButton').addEventListener('click', function() {
+    // Création du nouvel élement
+    const div = document.createElement('div');
+    div.class = 'img-thumbnail';
+    const img = document.createElement('img');
+    img.src = document.querySelector('#urlImg').value;
+    div.appendChild(img);
+    // Ajout dans le noeud gallery
+    document.querySelector('#gallery').appendChild(div);
+});
