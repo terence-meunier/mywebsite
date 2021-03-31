9e6205b095d850b92aaaad06f9a28a70724e6acf
Térence <terence.meunier@gmail.com>
2021-03-31 09:08:49 +0200

    feat(delete picture): delete picture fixes #20

--- resources/js/gallery.js
+++ resources/js/gallery.js
@@ -37,13 +37,66 @@ document.querySelector('#addImgForm').addEventListener('click', () => toggleForm
 
 // Récupération de l'url dans le champ de texte quand on clique sur le bouton du formulaire
 document.querySelector('#addImgButton').addEventListener('click', function() {
-    // Création du nouvel élement
+    // Création du nouvel élement de type div
     const div = document.createElement('div');
-    div.class = 'img-thumbnail';
+    // Ajouter la classe img-thumbnail
+    div.className = 'img-thumbnail';
+    // Créé la div pour le suppression de l'élément
+    const divDelete = document.createElement('div');
+    // Ajouter une classe delete-img a la div
+    divDelete.className = "delete-img";
+    // Masquer la croix du delete
+    divDelete.style = "display: none";
+    // Ajouter le bouton de suppression
+    const buttonDelete = document.createElement('button');
+    // Ajouter la classe main-button au bouton delete
+    buttonDelete.className = "main-button";
+    // Ajouter le contenu du bouton
+    buttonDelete.textContent = "X";
+    // Ajout de l'event listener sur le bouton delete
+    buttonDelete.addEventListener('click', function() {
+        this.parentElement.parentElement.remove();
+    });
+    // Ajouter le bouton dans la div delete
+    divDelete.appendChild(buttonDelete);
+    // Ajouter la div delete dans la div principale de l'image
+    div.appendChild(divDelete);
+    // Ajout de l'image elle-même
     const img = document.createElement('img');
     img.src = document.querySelector('#urlImg').value;
+    // Ajout de l'image dans le div principal de l'image
     div.appendChild(img);
-    // Ajout dans le noeud gallery
+    // Ajout de la div principale dans la balise gallery
     document.querySelector('#gallery').appendChild(div);
+    // On referme la div du formulaire d'ajout d'image
     toggleFormImg();
 });
+
+// Add event listener for the delete img button
+document.querySelectorAll('.delete-img').forEach(function (element) {
+    element.querySelector('button').addEventListener('click', function() {
+        this.parentElement.parentElement.remove();
+    });
+});
+
+// Set deleteImg display none
+document.querySelectorAll('.delete-img').forEach(function(element) {
+    element.style = 'display: none';
+});
+
+let deleteImgIsVisible = false;
+// Toggle function for form visibility
+function toggleDeleteImg() {
+    if (deleteImgIsVisible) {
+        document.querySelectorAll('.delete-img').forEach(element => element.style = "display: none");
+        document.querySelector('#deleteImg').textContent = 'Delete a picture';
+    } else {
+        document.querySelectorAll('.delete-img').forEach(element => element.style = "display: block");
+        document.querySelector('#deleteImg').textContent = 'Cancel';
+    }
+
+    deleteImgIsVisible = !deleteImgIsVisible;
+}
+
+// Add event listener sur le bouton deleteImg 
+document.querySelector('#deleteImg').addEventListener('click', () => toggleDeleteImg());
